e39b6375aa6288550dffb846bdd188456703d826
danghungthai <danghungthai.41@gmail.com>
2023-07-02 20:01:28 +0700

    feature/authorization

--- internal/middlewares/authorization.go
+++ internal/middlewares/authorization.go
@@ -1,3 +1,24 @@
 package middleware
 
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+func extractTokenFromHeader(r *http.Request) (string, error) {
+
+	return "", nil
+
+}
+
+func RequestedAuth() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		token, err := extractTokenFromHeader(c.Request)
+		if err != nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		}
+		c.JSON(http.StatusOK, gin.H{"data": token})
+
+	}
+}
